91a83a2b5e13e1aba26b85845a17abf200f550f1
Ankit-Shekam <ankit-shekam@users.noreply.github.com>
2020-06-02 20:10:18 +0530

    final commit

---
+++ public/serviceworker.js
@@ -0,0 +1,48 @@
+// cache is the browser memory allotted on the system 
+const CACHE_NAME = "version-1";
+const urlsToCache = ['index.html', 'offline.html'];
+
+const self = this;
+
+// initial install
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('open cache');
+
+                return cache.addAll(urlsToCache);
+            })
+    )
+});
+
+
+// listen to request
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(() => {
+                return fetch(event.request)
+                    .catch(() => caches.match('offline.html'))
+            })
+    )
+});
+
+
+// activate the service worker
+self.addEventListener('activate', (event) => {
+    const cacheWhiteList = [];
+    cacheWhiteList.push(CACHE_NAME);
+    
+    event.waitUntil(
+        caches.keys()
+            .then((cacheNames) => Promise.all(
+                cacheNames.map((cacheNames) => {
+                    if(!cacheWhiteList.includes(cacheNames)){
+                        return caches.delete(cacheNames);
+                    }
+                })
+            ))        
+    )
+
+});
